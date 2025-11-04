a18172533e011b901648ed69c308ff6efb0a53bb
god-s-perfect-idiot <samarmusthafa2014@gmail.com>
2025-11-04 23:43:45 +0530

    Implement initial project structure and setup

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import '../global.css';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="bg-[#000] flex-1 p-4">
+      <Text className="text-white text-[2.25rem] serif">Unimpressive Apps</Text>
+    </View>
+  );
+}
+
+
